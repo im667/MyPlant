e58b481b18ac9223c3cd1934db7ffa025d7f963c
im667 <koh9037@gmail.com>
2022-04-23 22:46:09 +0900

    ADD: AlertViewController

---
+++ MyPlant/VC/AlertViewController.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  MyPlant
+//
+//  Created by mac on 2022/04/23.
+//
+
+
+import UIKit
+
+class AlertViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
